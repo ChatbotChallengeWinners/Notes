e363a7c73df730eb773c1f0cc6dbe4fd88343e73
Nihilianth <nihili4nth@gmail.com>
2025-05-16 10:58:26 +0200

    Use cloud model for embeddings. Shows links to context in the sidebar

--- code/test4_chroma_api.py
+++ code/test4_chroma_api.py
@@ -1,5 +1,7 @@
 
 # import pprint as pp
+import os
+from chromadb import Embeddings
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
@@ -15,19 +17,20 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
 from dotenv import load_dotenv
+import requests
 load_dotenv()
+
 # https://docs.hpc.gwdg.de/services/saia/index.html#embeddings
-# Rodi: couldn't get embeddings with API to work
-# embeddings = OpenAIEmbeddings(
-#     model="e5-mistral-7b-instruct",
-#     base_url="https://chat-ai.academiccloud.de/v1",
-#     api_key="50c2a8b3e1a0da4085ccc99ac0247692",
-#     encoding_format="float",
-#     embedding_ctx_length=4096)
+embeddings = OpenAIEmbeddings(
+    model="e5-mistral-7b-instruct",
+    base_url="https://chat-ai.academiccloud.de/v1",
+    encoding_format="float",
+    embedding_ctx_length=4096,
+    tiktoken_enabled=False)
 
 # Rodi: if you have ollama locally, you can use this
-mbed_model = "mxbai-embed-large"
-embeddings = OllamaEmbeddings(model=mbed_model)
+# mbed_model = "mxbai-embed-large"
+# embeddings = OllamaEmbeddings(model=mbed_model)
 
 
 def get_vectorstore_from_url(url):
@@ -35,20 +38,25 @@ def get_vectorstore_from_url(url):
     loader = WebBaseLoader(url)
     docs = loader.load()
 
-    text_splitter = RecursiveCharacterTextSplitter()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 4096,
+        chunk_overlap  = 200,
+        length_function = len)
     doc_chunks = text_splitter.split_documents(docs)
 
-    vector_store = Chroma.from_documents(doc_chunks, embeddings)
+    vector_store = Chroma.from_documents(doc_chunks, embeddings, persist_directory="./chroma", collection_name="animals")
 
     return vector_store
 
 
-def get_context_retriever_chain(vector_store, model_name="meta-llama-3.1-8b-instruct"):
+def get_context_retriever_chain(vector_store, model_name="meta-llama-3.1-8b-rag"):
     llm = ChatOpenAI(
         model=model_name,
         openai_api_base="https://chat-ai.academiccloud.de/v1",
     )
-    retriever = vector_store.as_retriever()
+    retriever = vector_store.as_retriever(
+        search_type="mmr", search_kwargs={"k": 6, "fetch_k": 30}
+    )
     prompt = ChatPromptTemplate.from_messages([
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}"),
@@ -59,13 +67,28 @@ def get_context_retriever_chain(vector_store, model_name="meta-llama-3.1-8b-inst
     return retriever_chain
 
 
-def get_conversational_rag_chain(retriever_chain, model_name="llama-3.3-70b-instruct"):
+def get_conversational_rag_chain(retriever_chain, model_name="meta-llama-3.1-8b-instruct"):  # "llama-3.3-70b-instruct"):
     llm = ChatOpenAI(
         model=model_name,
         openai_api_base="https://chat-ai.academiccloud.de/v1",
     )
     prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a helpful assistant. You use Markdown features and Mermaid diagram in your replies. Answer the user's questions based on the below context:\n\n{context}"),        
+        ("system", """You are a helpful assistant. You use Markdown features everywhere like **bold text**, _cursive text_, headings, details tags and Mermaid diagrams in your replies. 
+         For all mermaid graphs, use simple bracket notation like a[Chapter 1]. Make sure to use mermaid notation like in following example:
+
+         ```mermaid
+        flowchart LR
+            a[Chapter 1] --> b[Chapter 2] --> c[Chapter 3]
+            c-->d[Using a chair]
+            subgraph TestSubgraph
+                c--"Using a knife"-->x[Hello there]
+            end
+            d-->f[Chapter 4]
+            e-->f
+        ```
+         
+         Answer the user's questions based on the below context:
+         {context}"""),        
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}"),
     ])
@@ -74,6 +97,7 @@ def get_conversational_rag_chain(retriever_chain, model_name="llama-3.3-70b-inst
 
 
 if __name__ == "__main__":
-    res = get_vectorstore_from_url('https://python.langchain.com/docs/integrations/vectorstores/chroma/')
+    # res = get_vectorstore_from_url('https://python.langchain.com/docs/integrations/vectorstores/chroma/')
     # pp.pp(res, indent=2)
+    res = get_vectorstore_from_url('https://gitlab-ce.gwdg.de/hpc-team-public/chat-ai-llamaindex-examples/-/raw/main/README.md?ref_type=heads')
 
--- code/test4_langchain_history_chroma_api.py
+++ code/test4_langchain_history_chroma_api.py
@@ -5,9 +5,7 @@ from test4_chroma_api import get_context_retriever_chain, get_vectorstore_from_u
 
 from langchain_core.messages import AIMessage, HumanMessage
 from streamlit_markdown import st_markdown
-from langchain.globals import set_debug
 
-set_debug(True)
 load_dotenv()
 
 # with st.spinner("Loading model"):
@@ -28,13 +26,13 @@ if 'chat_history' not in st.session_state:
 
 with st.sidebar:
     web_url = st.text_input("Enter URL")
-    st.write(st.session_state.chat_history)
+    # st.write(st.session_state.chat_history)
 
 # Streamlit re-runs the script. Show all messages
 for message in st.session_state.chat_history:
     role = isinstance(message, AIMessage) and "assistant" or "user"
     with st.chat_message(role):
-        st.markdown(message.content)
+        st_markdown(message.content)
 
 # retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
 # conv_rag_chain = get_conversational_rag_chain(retriever_chain)
@@ -44,23 +42,29 @@ for message in st.session_state.chat_history:
 def get_response(prompt: str):
     # print("get_response")
     # not very optimal (move to backend)
-    with st.spinner("Loading vectorstore"):
+    with st.spinner("Creating vector DB"):
         vector_store = get_vectorstore_from_url([
             'https://gist.githubusercontent.com/ChristopherA/bffddfdf7b1502215e44cec9fb766dfd/raw/e1e7fcd99cad49c8b9effdb6f876f3ad44239c1a/Mermaid_on_Github_Examples.md',
-
+            'https://de.wikipedia.org/wiki/Systematik_der_vielzelligen_Tiere',
             'https://en.wikipedia.org/wiki/List_of_animal_classes',
+            'https://en.wikipedia.org/wiki/Acanthocephala',
+            'https://en.wikipedia.org/wiki/Acoelomorpha',
+            'https://en.wikipedia.org/wiki/Annelida',
+            'https://en.wikipedia.org/wiki/Chordate',
+            'https://en.wikipedia.org/wiki/Echinodermata',
+            'https://en.wikipedia.org/wiki/Tardigrada',
             ])
-    with st.spinner("getting context"):
+    with st.spinner("Setting up Chains"):
         retriever_chain = get_context_retriever_chain(vector_store)
         conv_rag_chain = get_conversational_rag_chain(retriever_chain)
-    with st.spinner("running query"):
+    with st.spinner("Executing Prompt"):
         # print(st.session_state.chat_history)
         response = conv_rag_chain.invoke({
             "chat_history": st.session_state.chat_history,
             "input": prompt
         })
         print(response)
-        return response["answer"]
+        return response
 
 # Print user prompt and save to history
 if prompt := st.chat_input("Enter your prompt..."):
@@ -69,7 +73,11 @@ if prompt := st.chat_input("Enter your prompt..."):
 
     response = get_response(prompt)
 
-    st_markdown(response)
+    st_markdown(response["answer"])
     # print(response)
     st.session_state.chat_history.append(HumanMessage(prompt))
-    st.session_state.chat_history.append(AIMessage(response))
+    st.session_state.chat_history.append(AIMessage(response["answer"]))
+    with st.sidebar:
+        for idx, c in enumerate(response["context"]):
+            if c.metadata:
+                st.markdown(f'{idx}: [{c.metadata['title']}]({c.metadata['source']})')
